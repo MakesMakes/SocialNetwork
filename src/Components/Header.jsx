04bbd47733c5b3eeb15d6185240b6fb3b64dae55
MakesMakes <max.morozovts2012@yandex.ru>
2019-08-21 14:55:48 +0300

    1. Add Grid and Component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Rockstar_Games_Logo.svg/2000px-Rockstar_Games_Logo.svg.png" />
+    </header>
+  );
+};
+
+export default Header;
