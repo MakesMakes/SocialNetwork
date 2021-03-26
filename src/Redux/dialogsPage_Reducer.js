30ec22de147c2e00b0793ebaed17e0bc6236849b
Максим Морозов <max.morozovts2012@yandex.ru>
2021-03-26 17:23:48 +0300

    Тест комитов, внёс изменения

--- src/Redux/dialogsPage_Reducer.js
+++ src/Redux/dialogsPage_Reducer.js
@@ -4,7 +4,7 @@ const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 let initialState = {
     dialogs: [
         { id: 1, name: 'Andrey', img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/1d/1d8824774fe60418072441f4e6de2fca3ea5fa1b_full.jpg' },
-        { id: 2, name: 'Valakas', img: 'https://avatanplus.com/files/resources/original/5d3173fac59b316c092d0bb9.png' },
+        { id: 2, name: 'Valakasas', img: 'https://avatanplus.com/files/resources/original/5d3173fac59b316c092d0bb9.png' },
         { id: 3, name: 'Kinch', img: 'https://cdn.freelance.ru/img/portfolio/pics/00/35/26/3483153.jpg?mt=4408aae2' },
         { id: 4, name: 'Vasya Voron', img: 'https://yt3.ggpht.com/a/AGF-l79mPFS2qUdPRLbnEU4ZuvqEIo8w_eupgFhRKQ=s900-c-k-c0xffffffff-no-rj-mo' },
         { id: 5, name: 'Denchik', img: 'https://yt3.ggpht.com/a/AATXAJyPeXJCsU5gzf7X5klg09ZMlRw1eWLcIxAJYA=s900-c-k-c0xffffffff-no-rj-mo' },
