dce0d8a9ef74948059835d4f7fd22db0f34fc2a4
Максим Морозов <max.morozovts2012@yandex.ru>
2021-05-04 18:29:28 +0300

    #75 redux-form

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -4,12 +4,14 @@ import dialogsPageReducer from './dialogsPage_Reducer';
 import profilePageReducer from './profilePage_Reducer';
 import usersPageReducer from "./usersPage_Reducer";
 import thunkMiddleware from 'redux-thunk'
+import {reducer as formReducer} from 'redux-form'
 
 let reducers = combineReducers({
     dialogsPage: dialogsPageReducer,
     profilePage: profilePageReducer,
     usersPage: usersPageReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
